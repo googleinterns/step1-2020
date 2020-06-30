734d68f33f150103401bd4c58d4912417e1ca749
Kayla <kaylamyhome@gmail.com>
2020-06-30 14:50:55 -0400

    Test

---
+++ src/main/java/com/google/step/YOUR_PROJECT_NAME_HERE/servlets/Card.java
@@ -0,0 +1,16 @@
+package com.google.step.YOUR_PROJECT_NAME_HERE.servlets;
+
+class Card {
+
+  private final String heading;
+  private final String link;
+  private final String desc;
+  private final String code;
+
+  public Card(String heading, String link, String desc, String code) {
+    this.heading = heading;
+    this.link = link;
+    this.desc = desc;
+    this.code = code;
+  }
+}
