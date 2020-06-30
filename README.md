6cb3c23d6ced7f6b16a5ea63cbc4d6824ef73121
Kayla <kaylamyhome@gmail.com>
2020-06-30 14:14:01 -0400

    Implement link retrieval

--- README.md
+++ README.md
@@ -5,3 +5,16 @@ in Google's Student Training in Engineering program.
 
 To run this project, do `mvn package appengine:run` in the project root
 directory.
+
+## GitHub Checks
+
+This repository runs checks on every pull request and commit. You can run these
+locally from the root directory of the project.
+
+- Java Continuous Integration: Run `mvn package`
+- Java Format: Follow the instructions at
+  https://github.com/google/google-java-format. Note that this formats your code
+  without telling you the changes first.
+- JavaScript Lint:
+  - One time setup: run `npm install`
+  - Run `./node_modules/.bin/eslint .`
--- src/main/java/com/google/step/YOUR_PROJECT_NAME_HERE/servlets/SearchServlet.java
+++ src/main/java/com/google/step/YOUR_PROJECT_NAME_HERE/servlets/SearchServlet.java
@@ -15,23 +15,65 @@
 package com.google.step.YOUR_PROJECT_NAME_HERE.servlets;
 
 import java.io.IOException;
-import java.util.Arrays;
+import java.io.UnsupportedEncodingException;
+import java.net.URI;
+import java.net.URLEncoder;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.ServletException;
+import org.json.JSONObject;
 
 /** Servlet that handles searches. */
 @WebServlet("/search")
 public class SearchServlet extends HttpServlet {
 
+  private final HttpClient httpClient =
+      HttpClient.newBuilder().version(HttpClient.Version.HTTP_2).build();
+
+  private static String encodeValue(String value) {
+    try {
+      return URLEncoder.encode(value, StandardCharsets.UTF_8.toString());
+    } catch (UnsupportedEncodingException ex) {
+      throw new RuntimeException(ex.getCause());
+    }
+  }
+
   @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-    // Set some data on the request for the template to access.
-    request.setAttribute("previousQuery", request.getParameter("q"));
-    request.setAttribute("results", Arrays.asList("Aaron", "Iris", "Kayla"));
-    // Forward the request to the template (which is a servlet itself).
-    request.getRequestDispatcher("WEB-INF/templates/search.jsp").forward(request, response);
+  public void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws IOException, ServletException {
+
+    String query = encodeValue(request.getParameter("q"));
+
+    /** Send Request to W3 Restricted Google CSE */
+    String w3GoogleCSE =
+        "https://www.googleapis.com/customsearch/v1?key=AIzaSyCSKQy6t7HgEc1zI2thwgGncFVzyB8zZ00&cx=010448377421452380243:pat2rmwjvb8&q="
+            + query;
+
+    HttpRequest w3LinkRequest =
+        HttpRequest.newBuilder()
+            .GET()
+            .uri(URI.create(w3GoogleCSE))
+            .setHeader("User-Agent", "Java 11 HttpClient Bot")
+            .build();
+
+    try {
+      HttpResponse<String> w3LinkResponse =
+          httpClient.send(w3LinkRequest, HttpResponse.BodyHandlers.ofString());
+
+      JSONObject obj = new JSONObject(w3LinkResponse.body());
+      String w3Link = obj.getJSONArray("items").getJSONObject(0).getString("link");
+
+      response.setContentType("text/html;");
+      response.getWriter().println(w3Link);
+
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
   }
 }
