8b1bece2c88b73a137c7664a3317de605473c727
Kayla Leung <kaylaleung@google.com>
2020-07-07 13:08:37 -0400

    Remove empty initialization

--- src/main/java/com/google/step/YOUR_PROJECT_NAME_HERE/data/Card.java
+++ src/main/java/com/google/step/YOUR_PROJECT_NAME_HERE/data/Card.java
@@ -1,10 +1,10 @@
 package com.google.step.YOUR_PROJECT_NAME_HERE.data;
 
 public final class Card {
-  private String title = "";
-  private String code = "";
-  private String link = "";
-  private String description = "";
+  private String title;
+  private String code;
+  private String link;
+  private String description;
 
   public Card(String title, String code, String link, String description) {
     this.title = title;
