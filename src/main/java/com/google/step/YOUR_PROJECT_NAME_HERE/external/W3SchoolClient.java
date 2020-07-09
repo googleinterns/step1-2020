e7e4121aab56a504484529ac9d006cb369b9031c
Kayla Leung <kaylaleung@google.com>
2020-07-09 19:21:39 -0400

    Update variable name

--- src/main/java/com/google/step/YOUR_PROJECT_NAME_HERE/external/W3SchoolClient.java
+++ src/main/java/com/google/step/YOUR_PROJECT_NAME_HERE/external/W3SchoolClient.java
@@ -7,17 +7,17 @@ import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
 public final class W3SchoolClient {
-  private static final String TITLE_ID = "h1";
-  private static final String DESC_ID = "p";
-  private static final String SNIPPET_ID = "w3-example";
-  private static final String CODE_ID = "w3-code";
+  private static final String CARD_TITLE_ID = "h1";
+  private static final String CARD_DESC_ID = "p";
+  private static final String CARD_SNIPPET_ID = "w3-example";
+  private static final String CARD_CODE_ID = "w3-code";
 
   public Card search(String w3Link) throws IOException {
 
     Document doc = Jsoup.connect(w3Link).get();
-    Elements titles = doc.getElementsByTag(TITLE_ID);
-    Elements descriptions = doc.getElementsByTag(DESC_ID);
-    Elements snippets = doc.getElementsByClass(SNIPPET_ID);
+    Elements titles = doc.getElementsByTag(CARD_TITLE_ID);
+    Elements descriptions = doc.getElementsByTag(CARD_DESC_ID);
+    Elements snippets = doc.getElementsByClass(CARD_SNIPPET_ID);
 
     String title = "";
     String description = "";
@@ -26,7 +26,7 @@ public final class W3SchoolClient {
     if (!isNullOrEmpty(titles) && !isNullOrEmpty(descriptions) && !isNullOrEmpty(snippets)) {
       title = titles.first().text();
       description = descriptions.first().text();
-      code = snippets.first().getElementsByClass(CODE_ID).text();
+      code = snippets.first().getElementsByClass(CARD_CODE_ID).text();
     }
 
     if (!title.isEmpty() && !description.isEmpty() && !code.isEmpty()) {
