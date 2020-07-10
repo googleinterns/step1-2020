18ead5161c1d66744acf46b40712f9aaca9a5f9d
Kayla Leung <kaylaleung@google.com>
2020-07-10 18:23:16 -0400

    Change variable names, update dependency

--- src/main/java/com/google/step/YOUR_PROJECT_NAME_HERE/external/W3SchoolClient.java
+++ src/main/java/com/google/step/YOUR_PROJECT_NAME_HERE/external/W3SchoolClient.java
@@ -7,36 +7,33 @@ import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
 public final class W3SchoolClient {
-  private static final String CARD_TITLE_ID = "h1";
-  private static final String CARD_DESC_ID = "p";
-  private static final String CARD_SNIPPET_ID = "w3-example";
-  private static final String CARD_CODE_ID = "w3-code";
+  private static final String TITLE_TAG = "h1";
+  private static final String DESC_TAG = "p";
+  private static final String SNIPPET_CLASS = "w3-example";
+  private static final String CODE_CLASS = "w3-code";
 
   public Card search(String w3Link) {
-    Document doc;
+    Document doc = null;
     try {
       doc = Jsoup.connect(w3Link).get();
-      Elements titles = doc.getElementsByTag(CARD_TITLE_ID);
-      Elements descriptions = doc.getElementsByTag(CARD_DESC_ID);
-      Elements snippets = doc.getElementsByClass(CARD_SNIPPET_ID);
-
-      if (titles.isEmpty() || titles.first().text().isEmpty()) {
-        return null;
-      }
-      if (descriptions.isEmpty() || descriptions.first().text().isEmpty()) {
-        return null;
-      }
-      if (snippets.isEmpty()
-          || snippets.first().getElementsByClass(CARD_CODE_ID).text().isEmpty()) {
-        return null;
-      }
-      String title = titles.first().text();
-      String description = descriptions.first().text();
-      String code = snippets.first().getElementsByClass(CARD_CODE_ID).text();
-
-      return new Card(title, code, w3Link, description);
     } catch (IOException e) {
       return null;
     }
+    Elements titles = doc.getElementsByTag(TITLE_TAG);
+    if (titles.isEmpty() || titles.first().text().isEmpty()) {
+      return null;
+    }
+    Elements descriptions = doc.getElementsByTag(DESC_TAG);
+    if (descriptions.isEmpty() || descriptions.first().text().isEmpty()) {
+      return null;
+    }
+    Elements snippets = doc.getElementsByClass(SNIPPET_CLASS);
+    if (snippets.isEmpty() || snippets.first().getElementsByClass(CODE_CLASS).text().isEmpty()) {
+      return null;
+    }
+    String title = titles.first().text();
+    String description = descriptions.first().text();
+    String code = snippets.first().getElementsByClass(CODE_CLASS).text();
+    return new Card(title, code, w3Link, description);
   }
 }
