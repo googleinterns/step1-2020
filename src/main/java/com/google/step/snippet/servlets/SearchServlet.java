e8b03cced7ccddf65f449b2f3a6e3ac6090e92e6
AaronLopes <aaronlopes@me.com>
2020-07-13 13:34:58 -0700

    edit for style check

--- src/main/java/com/google/step/snippet/servlets/SearchServlet.java
+++ src/main/java/com/google/step/snippet/servlets/SearchServlet.java
@@ -102,7 +102,6 @@ public class SearchServlet extends HttpServlet {
      */
     String geeksLink = getLink(GEEKS_CSE_ID, query);
     if (geeksLink != null) {}
-    
     request.setAttribute("card_list", allCards);
     request.getRequestDispatcher("WEB-INF/templates/search.jsp").forward(request, response);
   }
