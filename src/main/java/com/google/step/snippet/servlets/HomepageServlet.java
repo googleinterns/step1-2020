82b31c06aa5205b9c19a13e32fab872fe29cf7a7
AaronLopes <aaronlopes@me.com>
2020-07-14 10:56:44 -0700

    remove import

--- src/main/java/com/google/step/snippet/servlets/HomepageServlet.java
+++ src/main/java/com/google/step/snippet/servlets/HomepageServlet.java
@@ -15,7 +15,6 @@
 package com.google.step.snippet.servlets;
 
 import java.io.IOException;
-import java.util.Date;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
