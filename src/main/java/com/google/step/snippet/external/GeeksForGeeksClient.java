c6c1c67c3fd82dcc1e077e10010c3b6487d5a88d
AaronLopes <aaronlopes@me.com>
2020-07-14 14:45:59 -0700

    add geeks scraper

---
+++ src/main/java/com/google/step/snippet/external/GeeksForGeeksClient.java
@@ -0,0 +1,39 @@
+package com.google.step.snippet.external;
+
+import com.google.step.snippet.data.Card;
+import java.io.IOException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+public final class GeeksForGeeksClient {
+  private static final String TITLE_TAG = "h1";
+  private static final String DESC_TAG = "p";
+  private static final String SNIPPET_CLASS = "code-block";
+  private static final String CODE_CLASS = "code-container";
+
+  public Card search(String geeksLink) {
+    Document doc = null;
+    try {
+      doc = Jsoup.connect(geeksLink).get();
+    } catch (IOException e) {
+      return null;
+    }
+    Elements titles = doc.getElementsByTag(TITLE_TAG);
+    if (titles.isEmpty() || titles.first().text().isEmpty()) {
+      return null;
+    }
+    Elements descriptions = doc.getElementsByTag(DESC_TAG);
+    if (descriptions.isEmpty() || descriptions.first().text().isEmpty()) {
+      return null;
+    }
+    Elements snippets = doc.getElementsByClass(SNIPPET_CLASS);
+    if (snippets.isEmpty() || snippets.first().getElementsByClass(CODE_CLASS).text().isEmpty()) {
+      return null;
+    }
+    String title = titles.first().text();
+    String description = descriptions.first().text();
+    String code = snippets.first().getElementsByClass(CODE_CLASS).text();
+    return new Card(title, code, geeksLink, description);
+  }
+}
