89774533c29ca8da78b5a3682476eb13f2f1d70d
Kayla <kaylaleung@google.com>
2020-07-16 10:30:34 -0400

    Add static imports

--- src/test/java/com/google/step/snippet/W3SchoolClientTest.java
+++ src/test/java/com/google/step/snippet/W3SchoolClientTest.java
@@ -1,8 +1,10 @@
 package com.google.step.snippet;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
 import com.google.step.snippet.data.Card;
 import com.google.step.snippet.external.W3SchoolClient;
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -10,7 +12,7 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public final class W3SchoolClientTest {
 
-  W3SchoolClient client = new W3SchoolClient();
+  private final W3SchoolClient client = new W3SchoolClient();
 
   @Test
   public void htmlImage() {
@@ -21,7 +23,7 @@ public final class W3SchoolClientTest {
             "<img src=\"img_girl.jpg\" alt=\"Girl in a jacket\" width=\"500\" height=\"600\">",
             "https://www.w3schools.com/tags/tag_img.asp",
             "How to insert an image:");
-    Assert.assertTrue(actual.equals(expected));
+    assertEquals(actual, expected);
   }
 
   @Test
@@ -34,27 +36,24 @@ public final class W3SchoolClientTest {
                 + " JSON.stringify(myObj); window.location = \"demo_json.php?x=\" + myJSON;",
             "https://www.w3schools.com/js/js_json_intro.asp",
             "JSON: JavaScript Object Notation.");
-    Assert.assertTrue(actual.equals(expected));
+    assertEquals(actual, expected);
   }
 
   @Test
   public void incompleteCard() {
     Card actual = client.search("https://www.w3schools.com/html/html_quiz.asp");
-    Card expected = null;
-    Assert.assertEquals(actual, expected);
+    assertNull(actual);
   }
 
   @Test
   public void invalidLink() {
     Card actual = client.search("https://www.w3schools.com/css/html.asp");
-    Card expected = null;
-    Assert.assertEquals(actual, expected);
+    assertNull(actual);
   }
 
   @Test
   public void blankPage() {
     Card actual = client.search("https://www.w3schools.com/");
-    Card expected = null;
-    Assert.assertEquals(actual, expected);
+    assertNull(actual);
   }
 }
