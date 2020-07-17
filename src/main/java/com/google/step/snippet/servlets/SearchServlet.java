c0c3d1ff9297a9c87f9b902d911ec3551eed9013
AaronLopes <aaronlopes@me.com>
2020-07-17 02:06:10 -0700

    fix format

--- src/main/java/com/google/step/snippet/servlets/SearchServlet.java
+++ src/main/java/com/google/step/snippet/servlets/SearchServlet.java
@@ -30,8 +30,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.Callable;
-import java.util.concurrent.CancellationException
-import java.util.concurrent.ExecutionException
+import java.util.concurrent.CancellationException;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.RejectedExecutionException;
