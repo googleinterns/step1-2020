bfef208ed2de47bd3b0a6edb7d5932724e8fd5a3
Kayla <kaylaleung@google.com>
2020-07-21 18:07:18 -0400

    Set source name constant

--- src/main/java/com/google/step/snippet/external/StackOverflowClient.java
+++ src/main/java/com/google/step/snippet/external/StackOverflowClient.java
@@ -37,6 +37,7 @@ public final class StackOverflowClient implements Client {
   private static final String BODY_PARAMETER = "body";
   private static final String CODE_PARAMETER = "code";
   private static final String ANSWER_ID_PARAMETER = "answer_id";
+  private static final String SOURCE_NAME = "StackOverflow";
   // Set 200 to be the maximum length of description for MVP.
   private static final int MAX_DESCRIPTION_LENGTH = 200;
 
@@ -75,7 +76,7 @@ public final class StackOverflowClient implements Client {
     // No description or code is allowed for StackOverflow.
     String description = getDescription(answerBody);
     String code = getCode(answerBody);
-    return new Card(title, code, url, description, "StackOverflow");
+    return new Card(title, code, url, description, SOURCE_NAME);
   }
 
   /* Get the question id of passed in URL. */
--- src/main/java/com/google/step/snippet/external/W3SchoolsClient.java
+++ src/main/java/com/google/step/snippet/external/W3SchoolsClient.java
@@ -11,6 +11,7 @@ public final class W3SchoolsClient implements Client {
   private static final String DESC_TAG = "p";
   private static final String SNIPPET_CLASS = "w3-example";
   private static final String CODE_CLASS = "w3-code";
+  private static final String SOURCE_NAME = "W3Schools";
 
   private String cseId = null;
 
@@ -52,6 +53,6 @@ public final class W3SchoolsClient implements Client {
     String title = titles.first().text();
     String description = descriptions.first().text();
     String code = snippets.first().getElementsByClass(CODE_CLASS).text();
-    return new Card(title, code, w3Link, description, "W3Schools");
+    return new Card(title, code, w3Link, description, SOURCE_NAME);
   }
 }
