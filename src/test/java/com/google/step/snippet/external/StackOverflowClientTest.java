daf52a25236a4957da06baf1235804d4908fe2ec
Iris Liu <jiayiliu@live.unc.edu>
2020-07-22 22:56:56 -0400

    add card test

--- src/test/java/com/google/step/snippet/external/StackOverflowClientTest.java
+++ src/test/java/com/google/step/snippet/external/StackOverflowClientTest.java
@@ -59,47 +59,68 @@ public final class StackOverflowClientTest {
   }
 
   @Test
+  public void getAnswerBodyTest() {
+    // Valid answer id.
+    String answer_id1 = "218510";
+    // Invalid answer id.
+    String answer_id2 = "Answer id";
+
+    String answer_body1 = client.getAnswerBody(answer_id1);
+    String answer_body2 = client.getAnswerBody(answer_id2);
+
+    assertEquals(
+        "<p>When you declare a reference variable (i.e. an object) you are really"
+            + " creating a pointer to an object. Consider the following code where you declare a variable of"
+            + " primitive type <code>int</code>:</p>\n\n"
+            + "<pre><code>int x;\n"
+            + "x = 10;\n"
+            + "</code></pre>\n\n"
+            + "<p>In this example, the variable <code>x</code> i",
+        answer_body1);
+    assertNull(answer_body2);
+  }
+
+  @Test
   public void validCardTest1() {
     Card actual =
         client.search(
             "https://stackoverflow.com/questions/22250067/how-to-get-address-of-a-pointer-in-c-c");
-
-    assertEquals("How to get address of a pointer in c/c++?", actual.getTitle());
-    assertNull(actual.getCode());
-    assertEquals(
-        "https://stackoverflow.com/questions/22250067/how-to-get-address-of-a-pointer-in-c-c",
-        actual.getLink());
-    assertEquals(
-        "<p>To get the address of p do:</p>\n\n"
-            + "<pre><code>int **pp = &amp;p;\n"
-            + "</code></pre>\n\n"
-            + "<p>and you can go on:</p>\n\n"
-            + "<pre><code>int ***ppp = &amp;pp;\n"
-            + "int ****pppp = &amp;ppp;\n"
-            + "...\n"
-            + "</code></pre>\n\n"
-            + "<p>or, only in C++11, you can do:</p>\n\n"
-            + "<pre><code>auto pp = std::addressof(p);\n"
-            + "</code></pre>\n\n"
-            + "<p>To print the addre",
-        actual.getDescription().substring(0, 300));
+    Card expected =
+        new Card(
+            "How to get address of a pointer in c/c++?",
+            null,
+            "https://stackoverflow.com/questions/22250067/how-to-get-address-of-a-pointer-in-c-c",
+            "<p>To get the address of p do:</p>\n\n"
+                + "<pre><code>int **pp = &amp;p;\n"
+                + "</code></pre>\n\n"
+                + "<p>and you can go on:</p>\n\n"
+                + "<pre><code>int ***ppp = &amp;pp;\n"
+                + "int ****pppp = &amp;ppp;\n"
+                + "...\n"
+                + "</code></pre>\n\n"
+                + "<p>or, only in C++11, you can do:</p>\n\n"
+                + "<pre><code>auto pp = std::addressof(p);\n"
+                + "</code></pre>\n\n"
+                + "<p>To print the addre"
+                + "...");
+    assertEquals(expected, actual);
   }
 
   @Test
   public void validCardTest2() {
     Card actual =
         client.search("https://stackoverflow.com/questions/11928566/img-vs-image-tag-in-html");
-
-    assertEquals("&lt;img&gt; vs &lt;image&gt; tag in HTML", actual.getTitle());
-    assertNull(actual.getCode());
-    assertEquals(
-        "https://stackoverflow.com/questions/11928566/img-vs-image-tag-in-html", actual.getLink());
-    assertEquals(
-        "<p>Yes and no. As you point out <code>&lt;image&gt;</code> has been a synonym for"
-            + " <code>&lt;img&gt;</code> for a long time. I believe it was an early Netscape"
-            + " browser that first did this, possibly to compensate for user error, or possibly"
-            + " because there was dispute at the time whether the element sho",
-        actual.getDescription().substring(0, 300));
+    Card expected =
+        new Card(
+            "&lt;img&gt; vs &lt;image&gt; tag in HTML",
+            null,
+            "https://stackoverflow.com/questions/11928566/img-vs-image-tag-in-html",
+            "<p>Yes and no. As you point out <code>&lt;image&gt;</code> has been a synonym for"
+                + " <code>&lt;img&gt;</code> for a long time. I believe it was an early Netscape"
+                + " browser that first did this, possibly to compensate for user error, or possibly"
+                + " because there was dispute at the time whether the element sho"
+                + "...");
+    assertEquals(expected, actual);
   }
 
   @Test
