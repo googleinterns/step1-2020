7375ee84dcac94d884cebffee1789a62b79b389b
Kayla <kaylaleung@google.com>
2020-07-27 20:17:53 -0400

    Remove tests

--- src/test/java/com/google/step/snippet/external/W3SchoolsClientTest.java
+++ src/test/java/com/google/step/snippet/external/W3SchoolsClientTest.java
@@ -44,36 +44,6 @@ public final class W3SchoolsClientTest {
   }
 
   @Test
-  public void cssEscaping() {
-    Card actual = client.search("https://www.w3schools.com/html/html_css.asp", "css style");
-    Card expected =
-        new Card(
-            "HTML Styles - CSS",
-            "&lt;h1 style=&quot;color:blue;&quot;&gt;A Blue Heading&lt;/h1&gt; &lt;p"
-                + " style=&quot;color:red;&quot;&gt;A red paragraph.&lt;/p&gt;",
-            "https://www.w3schools.com/html/html_css.asp",
-            "CSS stands for Cascading Style Sheets.",
-            "W3Schools",
-            "https://w3schools.com/favicon.ico");
-    assertEquals(expected, actual);
-  }
-
-  @Test
-  public void divEscaping() {
-    Card actual = client.search("https://www.w3schools.com/css/css3_flexbox.asp", "css flexbox");
-    Card expected =
-        new Card(
-            "CSS Flexbox",
-            "&lt;div class=&quot;flex-container&quot;&gt; &lt;div&gt;1&lt;/div&gt;"
-                + " &lt;div&gt;2&lt;/div&gt; &lt;div&gt;3&lt;/div&gt; &lt;/div&gt;",
-            "https://www.w3schools.com/css/css3_flexbox.asp",
-            "A flex container with three flex items:",
-            "W3Schools",
-            "https://w3schools.com/favicon.ico");
-    assertEquals(expected, actual);
-  }
-
-  @Test
   public void partiallyFilledCard() {
     Card actual = client.search("https://www.w3schools.com/html/html_quiz.asp", "html quiz");
     assertNull(actual);
